5ce5e31df25ac5d919f9aef0e4b705398c0160e6
Astruum0 <arthurvella33@gmail.com>
2023-02-24 09:46:00 +0100

    first commit

---
+++ src/main/java/com/arthurvella/pizzachat/PizzachatApplication.java
@@ -0,0 +1,13 @@
+package com.arthurvella.pizzachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzachatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzachatApplication.class, args);
+	}
+
+}
