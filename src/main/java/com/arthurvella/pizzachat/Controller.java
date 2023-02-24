f0b042c5fe97e2325df3bbae1e1b56dc13769383
Astruum0 <arthurvella33@gmail.com>
2023-02-24 09:59:47 +0100

    hello world

--- src/main/java/com/arthurvella/pizzachat/Controller.java
+++ src/main/java/com/arthurvella/pizzachat/Controller.java
@@ -1,7 +1,9 @@
 package com.arthurvella.pizzachat;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Controller {
     @GetMapping("/")
     String HelloWord() {
